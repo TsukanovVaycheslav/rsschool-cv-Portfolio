b7c2ee0d84748f5a1facd4041c38668610ba1a8c
TsukanovVaycheslav <58068725+TsukanovVaycheslav@users.noreply.github.com>
2021-03-02 09:59:25 +0300

    Add files via upload

---
+++ js/languageSelection.js
@@ -0,0 +1,235 @@
+
+    function change1(){
+
+        menuEn();
+        //HtmlCssEn();
+       // jsEn();
+        //sharpEn();
+        latestProjectEn();
+        footerEn();
+
+        // Latest project / Последние проекты
+        document.getElementById("latestProject").innerHTML="Latest project";
+    }
+
+    function change2(){
+
+        menuRu();
+        // HtmlCssRu();
+        //jsRu();
+        //sharpRu();
+        latestProjectRu();
+        footerRu()
+        
+        // Latest project / Последние проекты
+        document.getElementById("latestProject").innerHTML="Последние проекты";
+
+        //document.getElementById("").innerHTML="";
+    }
+
+
+    // Latest project
+    // ---------------------------English-------------------------------------------------------
+
+    function menuEn() {
+        document.getElementById("projects").innerHTML="Projects";
+        document.getElementById("specialty").innerHTML="RS school-cv";
+        document.getElementById("firstName").innerHTML="Vyacheslav Tsukanov";
+        document.getElementById("description").innerHTML="I love creating projects that have a positive impact";
+
+    }
+
+    function HtmlCssEn() {
+        // HTML CSS
+     
+    }
+
+    function jsEn() {
+        // JS
+     
+    }
+
+    function sharpEn() {
+        // Sharp
+
+    }
+
+
+    function latestProjectEn() {
+        document.getElementById("more1").innerHTML="More";
+        document.getElementById("more2").innerHTML="More";
+        document.getElementById("more3").innerHTML="More";
+        document.getElementById("more4").innerHTML="More";
+        document.getElementById("more5").innerHTML="More";
+        document.getElementById("more6").innerHTML="More";
+        document.getElementById("more7").innerHTML="More";
+
+        document.getElementById("websiteWithATemplate1").innerHTML="Website with a template";
+        document.getElementById("websiteWithATemplate2").innerHTML="Website with a template";
+        document.getElementById("websiteWithATemplate4").innerHTML="Website with a template";
+        document.getElementById("websiteWithATemplate5").innerHTML="Website with a template";
+        document.getElementById("websiteWithATemplate7").innerHTML="Website with a template";
+
+        document.getElementById("game3").innerHTML="Game";
+        document.getElementById("game6").innerHTML="Game";
+
+        document.getElementById("theMethodsUsed3").innerHTML="The methods used:";
+        document.getElementById("theMethodsUsed6").innerHTML="The methods used:";
+
+        document.getElementById("deadlines1").innerHTML="deadlines";
+        document.getElementById("deadlines2").innerHTML="deadlines";
+        document.getElementById("deadlines3").innerHTML="deadlines";
+        document.getElementById("deadlines4").innerHTML="deadlines";
+        document.getElementById("deadlines5").innerHTML="deadlines";
+        document.getElementById("deadlines6").innerHTML="deadlines";
+        document.getElementById("deadlines7").innerHTML="deadlines";
+
+        document.getElementById("complexity1").innerHTML="complexity";
+        document.getElementById("complexity2").innerHTML="complexity";
+        document.getElementById("complexity3").innerHTML="complexity";
+        document.getElementById("complexity4").innerHTML="complexity";
+        document.getElementById("complexity5").innerHTML="complexity";
+        document.getElementById("complexity6").innerHTML="complexity";
+        document.getElementById("complexity7").innerHTML="complexity";
+
+        document.getElementById("description1").innerHTML="description";
+        document.getElementById("description2").innerHTML="description";
+        document.getElementById("description3").innerHTML="description";
+        document.getElementById("description4").innerHTML="description";
+        document.getElementById("description5").innerHTML="description";
+        document.getElementById("description6").innerHTML="description";
+        document.getElementById("description7").innerHTML="description";
+
+        document.getElementById("descriptionName1").innerHTML="Website - Organization of conferences";
+        document.getElementById("descriptionName2").innerHTML="Website - Couch Shop";
+        document.getElementById("descriptionName3").innerHTML="Game - 1001 Questions Game";
+        document.getElementById("descriptionName4").innerHTML="Website - Epa green";
+        document.getElementById("descriptionName5").innerHTML="Website - 'Space-X'";
+        document.getElementById("descriptionName6").innerHTML="Game - Cards Three in one";
+        document.getElementById("descriptionName7").innerHTML="Website - Always a delicious and varied menu for every taste and prosperity";
+
+        document.getElementById("workingDays1").innerHTML="5 working days";
+        document.getElementById("workingDays2").innerHTML="3 working days";
+        document.getElementById("workingDays3").innerHTML="15 working days";
+        document.getElementById("workingDays4").innerHTML="3 working days";
+        document.getElementById("workingDays5").innerHTML="4 working days";
+        document.getElementById("workingDays6").innerHTML="9 working days";
+        document.getElementById("workingDays7").innerHTML="5 working days";
+
+
+        document.getElementById("description1Proj1").innerHTML="Organization of conferences";
+        document.getElementById("description1Proj2").innerHTML="Large selection of couches";
+        document.getElementById("description1Proj3").innerHTML="The '1001 Questions Game' is a quiz contest in which participants must correctly answer a series of multiple-choice questions, to go to the next level.";
+        document.getElementById("description1Proj4").innerHTML="Landing Page templates";
+        document.getElementById("description1Proj5").innerHTML="Landing Page templates";
+        document.getElementById("description1Proj6").innerHTML="A puzzle is not an easy task to solve, which usually requires ingenuity, rather than high-level special knowledge. However some puzzles are stimulating theoretical and practical developments of scientists.";
+        document.getElementById("description1Proj7").innerHTML="While I wanted to redesign this app for my own personal use, there are other types of users that I should have been aware of when creating a universal design. Through the interview, I realized what pain points people who usually use an online service face, and solved these problems in my design.";
+    
+    }
+
+
+    function footerEn() {
+        // footer
+        document.getElementById("Communication").innerHTML="Communication";
+        document.getElementById("SocialNetwork").innerHTML="Social Network";
+    }
+
+
+    // -----------------------Russian------------------------------------------------------------
+
+    function menuRu() {
+        document.getElementById("projects").innerHTML="Проекты";
+        document.getElementById("specialty").innerHTML="RS school-cv";
+        document.getElementById("firstName").innerHTML="Цуканов Вячеслав";
+        document.getElementById("description").innerHTML="Я люблю создавать проекты, которые оказывают положительное влияние";
+  
+    }
+    
+    function HtmlCssRu() {
+        // HTML CSS
+   
+    }
+
+    function jsRu() {
+        // JS
+   
+    }
+
+    function sharpRu() {
+       
+    }
+
+    function latestProjectRu() {
+        document.getElementById("more1").innerHTML="Подробнее";
+        document.getElementById("more2").innerHTML="Подробнее";
+        document.getElementById("more3").innerHTML="Подробнее";
+        document.getElementById("more4").innerHTML="Подробнее";
+        document.getElementById("more5").innerHTML="Подробнее";
+        document.getElementById("more6").innerHTML="Подробнее";
+        document.getElementById("more7").innerHTML="Подробнее";
+
+        document.getElementById("websiteWithATemplate1").innerHTML="Сайт с шаблона";
+        document.getElementById("websiteWithATemplate2").innerHTML="Сайт с шаблона";
+        document.getElementById("websiteWithATemplate4").innerHTML="Сайт с шаблона";
+        document.getElementById("websiteWithATemplate5").innerHTML="Сайт с шаблона";
+        document.getElementById("websiteWithATemplate7").innerHTML="Сайт с шаблона";
+
+        document.getElementById("game3").innerHTML="Игра";
+        document.getElementById("game6").innerHTML="Игра";
+        document.getElementById("theMethodsUsed3").innerHTML="Используемые методы:";
+        document.getElementById("theMethodsUsed6").innerHTML="Используемые методы:";
+
+        document.getElementById("deadlines1").innerHTML="Сроки";
+        document.getElementById("deadlines2").innerHTML="Сроки";
+        document.getElementById("deadlines3").innerHTML="Сроки";
+        document.getElementById("deadlines4").innerHTML="Сроки";
+        document.getElementById("deadlines5").innerHTML="Сроки";
+        document.getElementById("deadlines6").innerHTML="Сроки";
+        document.getElementById("deadlines7").innerHTML="Сроки";
+
+        document.getElementById("complexity1").innerHTML="Сложность";
+        document.getElementById("complexity2").innerHTML="Сложность";
+        document.getElementById("complexity3").innerHTML="Сложность";
+        document.getElementById("complexity4").innerHTML="Сложность";
+        document.getElementById("complexity5").innerHTML="Сложность";
+        document.getElementById("complexity6").innerHTML="Сложность";
+        document.getElementById("complexity7").innerHTML="Сложность";
+
+        document.getElementById("description1").innerHTML="Описание";
+        document.getElementById("description2").innerHTML="Описание";
+        document.getElementById("description3").innerHTML="Описание";
+        document.getElementById("description4").innerHTML="Описание";
+        document.getElementById("description5").innerHTML="Описание";
+        document.getElementById("description6").innerHTML="Описание";
+        document.getElementById("description7").innerHTML="Описание";
+
+        document.getElementById("descriptionName1").innerHTML="Сайт - Организация конференций";
+        document.getElementById("descriptionName2").innerHTML="Сайт - Магазин кушеток";
+        document.getElementById("descriptionName3").innerHTML="Игра - 1001 вопрос";
+        document.getElementById("descriptionName4").innerHTML="Сайт - Epa green";
+        document.getElementById("descriptionName5").innerHTML="Сайт - 'Space-X'";
+        document.getElementById("descriptionName6").innerHTML="Игра - Карты три в одном";
+        document.getElementById("descriptionName7").innerHTML="Сайт - Всегда вкусное и разнообразное меню на любой вкус и достаток";
+
+        document.getElementById("workingDays1").innerHTML="5 рабочих дня";
+        document.getElementById("workingDays2").innerHTML="3 рабочих дня";
+        document.getElementById("workingDays3").innerHTML="15 рабочих дня";
+        document.getElementById("workingDays4").innerHTML="3 рабочих дня";
+        document.getElementById("workingDays5").innerHTML="4 рабочих дня";
+        document.getElementById("workingDays6").innerHTML="9 рабочих дня";
+        document.getElementById("workingDays7").innerHTML="5 рабочих дня";
+
+        document.getElementById("description1Proj1").innerHTML="Организация конференций";
+        document.getElementById("description1Proj2").innerHTML="Большой выбор кушеток";
+        document.getElementById("description1Proj3").innerHTML="Игра '1001 Questions Game' - это конкурс викторина, в котором участники должны правильно ответить на ряд вопросов с несколькими вариантами ответов, чтобы перейти на следующий уровень.";
+        document.getElementById("description1Proj4").innerHTML="Сайт с шаблона";
+        document.getElementById("description1Proj5").innerHTML="Сайт с шаблона";
+        document.getElementById("description1Proj6").innerHTML="Головоломка — непростая задача, для решения которой, как правило, требуется сообразительность, а не специальные знания высокого уровня. Тем не менее некоторые головоломки стимулируют теоретические и практические разработки учёных.";
+        document.getElementById("description1Proj7").innerHTML="Хотя я хотел перепроектировать это приложение для своего личного использования, есть и другие типы пользователей, о которых я должен был знать при создании универсального дизайна. Через интервью я понял, с какими болевыми точками сталкиваются люди, которые обычно пользуются онлайн сервисом, и решил эти проблемы в своем дизайне.";
+    }
+
+    function footerRu() {
+        // footer
+        document.getElementById("Communication").innerHTML="Cвязь";
+        document.getElementById("SocialNetwork").innerHTML="Соцсети";
+    }
